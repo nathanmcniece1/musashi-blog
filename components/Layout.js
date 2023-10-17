e46f4ac490f9114e39d06801d659d8a78e2ba055
nathanmcniece1 <nathanmcniece1@gmail.com>
2023-10-17 14:24:32 +1000

    new commit

--- components/Layout.js
+++ components/Layout.js
@@ -28,13 +28,29 @@ export default function Layout({ children, home }) {
       <header className={styles.header}>
         {home ? (
           <>
+            <Image
+              priority
+              src="/images/musashi.jpg"
+              className={utilStyles.borderCircle}
+              height={144}
+              width={144}
+              alt=""
+            />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
           </>
         ) : (
           <>
             <Link href="/">
+              <Image
+                priority
+                src="/images/musashi.jpg"
+                className={utilStyles.borderCircle}
+                height={108}
+                width={108}
+                alt=""
+              />
             </Link>
-            <h2 className={utilStyles.heading2Xl}>
+            <h2 className={utilStyles.headingLg}>
               <Link href="/" className={utilStyles.colorInherit}>
                 {name}
               </Link>
