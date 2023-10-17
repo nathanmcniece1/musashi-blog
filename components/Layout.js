8c5df8674584b24e37b4ff4079715c66340e4bf6
nathanmcniece1 <nathanmcniece1@gmail.com>
2023-10-17 15:17:46 +1000

    new commit

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'MUSASHI';
+const name = 'Musashi';
 export const siteTitle = 'Musashi Blog';
 
 export default function Layout({ children, home }) {
