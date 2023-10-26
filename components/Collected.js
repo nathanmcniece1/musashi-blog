b97c394bf699e3a999ce9f76de035a07f7f05bdd
nathanmcniece1 <nathanmcniece1@gmail.com>
2023-10-27 09:55:59 +1000

    v3

--- components/Collected.js
+++ components/Collected.js
@@ -3,6 +3,8 @@ import utilStyles from '../styles/utils.module.css';
 
 function Collected() {
     const [nfts, setNfts] = useState([]);
+    const [loading, setLoading] = useState(true); // Introduce a loading state
+    const numPlaceholders = 10; // Number of placeholder tiles
 
     useEffect(() => {
         const options = {
@@ -16,20 +18,25 @@ function Collected() {
         fetch('https://api.opensea.io/api/v2/chain/zora/account/0xb3C7AcA9728aB0355A1AFFfec5513D39a5986892/nfts', options)
             .then(response => response.json())
             .then(data => {
-
-                // console.log(data)
-                // Filter out the nfts with the name "LMAO.EXE"
                 const filteredNfts = data.nfts.filter(nft => nft.name !== "LMAO.EXE");
                 setNfts(filteredNfts);
+                setLoading(false); // Set loading to false once data is fetched
             })
             .catch(err => console.error(err));
-    }, []); // The empty array means this effect will only run once when the component mounts
+    }, []);
 
     return (
         <div className={utilStyles.nfts}>
-            {nfts.map((nft, index) => (
-                <img key={index} src={nft.image_url} alt={nft.name} />
-            ))}
+            {loading ? (
+                // Display multiple placeholders while loading
+                Array.from({ length: numPlaceholders }).map((_, index) => (
+                    <div key={index} className={utilStyles.placeholder}></div>
+                ))
+            ) : (
+                nfts.map((nft, index) => (
+                    <img key={index} src={nft.image_url} alt={nft.name} />
+                ))
+            )}
         </div>
     );
 }
--- pages/library.js
+++ pages/library.js
@@ -1,11 +1,15 @@
-import Layout from "../components/Layout";
+import Layout from '../components/Layout';
+import Tabs from '../components/Tabs';
+import utilStyles from '../styles/utils.module.css'; // Ensure you have the styles imported
 
 function Library(){
     return (
-        <>
-        <Layout>
+        <Layout home>
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px} ${utilStyles.border}`}>
+                <Tabs />
+                <h2 className={utilStyles.headingLg}>noteworthy books i’d like to recommend you, the internet.</h2>
+            </section>
         </Layout>
-        </>
     )
 }
 
