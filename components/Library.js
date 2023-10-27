bd2c58307bba7d69ab7f9a2b303482e2000fdcc3
nathanmcniece1 <nathanmcniece1@gmail.com>
2023-10-27 10:34:22 +1000

    v3

---
+++ components/Library.js
@@ -0,0 +1,25 @@
+import styles from '../styles/Library.module.css';
+
+function Library(){
+
+    const books = [
+        { title: "WAKING UP", author: "By Sam Harris" },
+        { title: "THE MORAL LANDSCAPE", author: "By Sam Harris" },
+        { title: "THE PROBLEMS OF PHILOSOPHY", author: "By Bertrand Russell" },
+        
+    ];
+
+
+    return (
+        <div className={styles.bookContainer}>
+            {books.map((book, index) => (
+                <div key={index} className={styles.bookTile}>
+                    <div className={styles.bookTitle}>{book.title}</div>
+                    <div className={styles.authorName}>{book.author}</div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Library;
--- pages/library.js
+++ pages/library.js
@@ -1,16 +1,19 @@
 import Layout from '../components/Layout';
 import Tabs from '../components/Tabs';
 import utilStyles from '../styles/utils.module.css'; // Ensure you have the styles imported
+import Library from '../components/Library'
 
-function Library(){
+
+function Libpage(){
     return (
         <Layout home>
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px} ${utilStyles.border}`}>
                 <Tabs />
-                <h2 className={utilStyles.headingLg}>noteworthy books i’d like to recommend you, the internet.</h2>
+                <h2 className={utilStyles.headingLg}>noteworthy books i’d like to recommend you, the internet</h2>
+                <Library />
             </section>
         </Layout>
     )
 }
 
-export default Library;
+export default Libpage;
