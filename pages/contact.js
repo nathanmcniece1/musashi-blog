bd2b0d638de5de1ce5d9092bdbf4cbc21d5e7ce0
nathanmcniece1 <nathanmcniece1@gmail.com>
2023-10-29 16:03:38 +1000

    v3

--- pages/contact.js
+++ pages/contact.js
@@ -7,6 +7,7 @@ function Contact() {
     <Layout home>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px} ${utilStyles.border}`}>
         <Tabs />
+        <h2 className={utilStyles.headingCl}>Have something to discuss? Get in touch.</h2>
         <div className={utilStyles.contactFormContainer}>
           <form className={utilStyles.contactForm}>
             <label htmlFor="name">Name:</label>
@@ -18,7 +19,7 @@ function Contact() {
             <label htmlFor="message">Message:</label>
             <textarea id="message" name="message" required></textarea>
 
-            <button type="submit">Send Message</button>
+            <button type="submit" className={utilStyles.sbmtBtn}>Send Message</button>
           </form>
         </div>
       </section>
